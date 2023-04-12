fe62a55d689f45d42535e9263635bb8b1b9ae5bb
Showrab15 <supto50showrab@gmail.com>
2023-04-12 20:55:34 +0600

    footer added

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Home from './components/Home/Home'
 import { Outlet } from 'react-router-dom'
 import Navbar from './components/Navbar/Navbar'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import Footer from './components/Footer/footer'
 function App() {
   const [count, setCount] = useState(0)
 
@@ -11,6 +12,7 @@ function App() {
     <div className="App">
 <Navbar></Navbar>
     <Outlet></Outlet>
+    <Footer></Footer>
     </div>
   )
 }
