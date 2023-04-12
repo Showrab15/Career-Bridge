532ba551c2fbaa3a8082725122bb44c397e74f38
Showrab15 <supto50showrab@gmail.com>
2023-04-12 22:13:05 +0600

    error page impliment done

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,9 @@
-import { useState } from 'react'
 import './App.css'
-import Home from './components/Home/Home'
 import { Outlet } from 'react-router-dom'
-import Navbar from './components/Navbar/Navbar'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import Footer from './components/Footer/footer'
-function App() {
-  const [count, setCount] = useState(0)
 
+function App() {
   return (
     <div className="App">
 <Navbar></Navbar>
